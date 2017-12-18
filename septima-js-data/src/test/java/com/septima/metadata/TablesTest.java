190815d32d38f7de2615495ae9f6a2df907192b3
mg <mg@altsoft.biz>
2017-12-18 10:27:07 +0300

    Tests environment

--- septima-js-data/src/test/java/com/septima/metadata/TablesTest.java
+++ septima-js-data/src/test/java/com/septima/metadata/TablesTest.java
@@ -1,8 +1,22 @@
 package com.septima.metadata;
 
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class TablesTest {
+
+    @BeforeClass
+    public static void setupDataSource() throws NamingException {
+        InitialContext ctx = new InitialContext();
+        DataSource dataSource = new JdbcDataSource();
+        ctx.bind("h2", dataSource);
+    }
+
 
     @Test
     public void easSchema() {
