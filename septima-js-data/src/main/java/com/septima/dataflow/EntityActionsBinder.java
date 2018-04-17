01351ed82375021ac5cefbff8a302b0e7e81586b
marat-gainullin <marat.gainullin.iv@gmail.com>
2018-04-17 23:24:44 +0300

    Ordering of data changes at single application entity level added

--- septima-js-data/src/main/java/com/septima/dataflow/EntityActionsBinder.java
+++ septima-js-data/src/main/java/com/septima/dataflow/EntityActionsBinder.java
@@ -1,19 +1,16 @@
 package com.septima.dataflow;
 
-import com.septima.metadata.Parameter;
 import com.septima.changes.*;
 import com.septima.entities.SqlEntity;
 import com.septima.jdbc.JdbcReaderAssigner;
 import com.septima.metadata.EntityField;
+import com.septima.metadata.Parameter;
 import com.septima.queries.SqlQuery;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,50 +18,48 @@ import java.util.stream.Collectors;
 
 public class EntityActionsBinder implements EntityActionsVisitor {
 
-    /**
-     * Short live information about sqlClause and its parameters.
-     * Its like {@link SqlQuery}, but much simpler.
-     * Performs parametrized prepared statements
-     * execution.
-     */
-    public static class BoundStatement {
-
-        private static final Logger QUERIES_LOGGER = Logger.getLogger(BoundStatement.class.getName());
-
-        private final String clause;
-        private final List<Parameter> parameters;
-        private final JdbcReaderAssigner jdbcReaderAssigner;
-
-        BoundStatement(String aClause, List<Parameter> aParameters, JdbcReaderAssigner aJdbcReaderAssigner) {
-            super();
-            clause = aClause;
-            parameters = aParameters;
-            jdbcReaderAssigner = aJdbcReaderAssigner;
-        }
-
-        public int apply(Connection aConnection) throws SQLException {
-            try (PreparedStatement stmt = aConnection.prepareStatement(clause)) {
-                for (int i = 0; i < parameters.size(); i++) {
-                    jdbcReaderAssigner.assignInParameter(parameters.get(i), stmt, i + 1, aConnection);
-                }
-                if (QUERIES_LOGGER.isLoggable(Level.FINE)) {
-                    QUERIES_LOGGER.log(Level.FINE, "Executing sql with {0} parameters: {1}", new Object[]{parameters.size(), clause});
-                }
-                return stmt.executeUpdate();
-            }
-        }
-    }
-
     private static final String INSERT_CLAUSE = "insert into %s (%s) values (%s)";
     private static final String DELETE_CLAUSE = "delete from %s where %s";
     private static final String UPDATE_CLAUSE = "update %s set %s where %s";
-
     private final List<BoundStatement> logEntries = new ArrayList<>();
     private final SqlEntity entity;
-
     public EntityActionsBinder(SqlEntity aEntity) {
         super();
         entity = aEntity;
+    }
+
+    private static String generatePlaceholders(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append("?");
+        }
+        return sb.toString();
+    }
+
+    private static String generateUpdateColumnsClause(List<Parameter> data) {
+        return data.stream()
+                .map(datum -> new StringBuilder(datum.getName()).append(" = ?"))
+                .reduce((name1, name2) -> name1.append(", ").append(name2))
+                .map(StringBuilder::toString)
+                .orElse("");
+    }
+
+    private static String generateInsertColumnsClause(List<Parameter> data) {
+        return data.stream()
+                .map(datum -> new StringBuilder(datum.getName()))
+                .reduce((name1, name2) -> name1.append(", ").append(name2))
+                .map(StringBuilder::toString)
+                .orElse("");
+    }
+
+    private static <E> List<E> concat(List<E> first, List<E> second) {
+        List<E> general = new ArrayList<>(first.size() + second.size());
+        general.addAll(first);
+        general.addAll(second);
+        return general;
     }
 
     public List<BoundStatement> getLogEntries() {
@@ -91,7 +86,7 @@ public class EntityActionsBinder implements EntityActionsVisitor {
         JdbcReaderAssigner jdbcReaderAssigner = entity.getDatabase().jdbcReaderAssigner(entity.isProcedure());
         aAdd.getData().entrySet().stream()
                 .map(asTableDatumEntry(entity))
-                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                .collect(Collectors.groupingBy(Map.Entry::getKey, LinkedHashMap::new,
                         Collectors.mapping(Map.Entry::getValue, Collectors.toList())))
                 .entrySet().stream()
                 .filter(entry ->
@@ -115,11 +110,11 @@ public class EntityActionsBinder implements EntityActionsVisitor {
         JdbcReaderAssigner jdbcReaderAssigner = entity.getDatabase().jdbcReaderAssigner(entity.isProcedure());
         Map<String, List<Parameter>> updatesKeys = aUpdate.getKeys().entrySet().stream()
                 .map(asTableDatumEntry(entity))
-                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                .collect(Collectors.groupingBy(Map.Entry::getKey, LinkedHashMap::new,
                         Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
         aUpdate.getData().entrySet().stream()
                 .map(asTableDatumEntry(entity))
-                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                .collect(Collectors.groupingBy(Map.Entry::getKey, LinkedHashMap::new,
                         Collectors.mapping(Map.Entry::getValue, Collectors.toList())))
                 .entrySet().stream()
                 .filter(entry -> !entry.getValue().isEmpty() && !updatesKeys.getOrDefault(entry.getKey(), List.of()).isEmpty() &&
@@ -151,7 +146,7 @@ public class EntityActionsBinder implements EntityActionsVisitor {
         JdbcReaderAssigner jdbcReaderAssigner = entity.getDatabase().jdbcReaderAssigner(entity.isProcedure());
         aRemove.getKeys().entrySet().stream()
                 .map(asTableDatumEntry(entity))
-                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                .collect(Collectors.groupingBy(Map.Entry::getKey, LinkedHashMap::new,
                         Collectors.mapping(Map.Entry::getValue, Collectors.toList())))
                 .entrySet().stream()
                 .filter(entry ->
@@ -188,17 +183,6 @@ public class EntityActionsBinder implements EntityActionsVisitor {
         ));
     }
 
-    private static String generatePlaceholders(int count) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            if (i > 0) {
-                sb.append(", ");
-            }
-            sb.append("?");
-        }
-        return sb.toString();
-    }
-
     /**
      * Generates sql where clause string for values array passed in. It's assumed
      * that key columns may not have NULL values. This assumption is made
@@ -215,26 +199,37 @@ public class EntityActionsBinder implements EntityActionsVisitor {
                 .orElse("");
     }
 
-    private static String generateUpdateColumnsClause(List<Parameter> data) {
-        return data.stream()
-                .map(datum -> new StringBuilder(datum.getName()).append(" = ?"))
-                .reduce((name1, name2) -> name1.append(", ").append(name2))
-                .map(StringBuilder::toString)
-                .orElse("");
-    }
+    /**
+     * Short live information about sqlClause and its parameters.
+     * Its like {@link SqlQuery}, but much simpler.
+     * Performs parametrized prepared statements
+     * execution.
+     */
+    public static class BoundStatement {
 
-    private static String generateInsertColumnsClause(List<Parameter> data) {
-        return data.stream()
-                .map(datum -> new StringBuilder(datum.getName()))
-                .reduce((name1, name2) -> name1.append(", ").append(name2))
-                .map(StringBuilder::toString)
-                .orElse("");
-    }
+        private static final Logger QUERIES_LOGGER = Logger.getLogger(BoundStatement.class.getName());
 
-    private static <E> List<E> concat(List<E> first, List<E> second) {
-        List<E> general = new ArrayList<>(first.size() + second.size());
-        general.addAll(first);
-        general.addAll(second);
-        return general;
+        private final String clause;
+        private final List<Parameter> parameters;
+        private final JdbcReaderAssigner jdbcReaderAssigner;
+
+        BoundStatement(String aClause, List<Parameter> aParameters, JdbcReaderAssigner aJdbcReaderAssigner) {
+            super();
+            clause = aClause;
+            parameters = aParameters;
+            jdbcReaderAssigner = aJdbcReaderAssigner;
+        }
+
+        public int apply(Connection aConnection) throws SQLException {
+            try (PreparedStatement stmt = aConnection.prepareStatement(clause)) {
+                for (int i = 0; i < parameters.size(); i++) {
+                    jdbcReaderAssigner.assignInParameter(parameters.get(i), stmt, i + 1, aConnection);
+                }
+                if (QUERIES_LOGGER.isLoggable(Level.FINE)) {
+                    QUERIES_LOGGER.log(Level.FINE, "Executing sql with {0} parameters: {1}", new Object[]{parameters.size(), clause});
+                }
+                return stmt.executeUpdate();
+            }
+        }
     }
 }
